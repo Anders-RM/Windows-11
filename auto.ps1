c3b2da9de507ea671e6d72a7cafe6a27a26efe59
Anders-RM <Anders_RMathiesen@pm.me>
2023-06-04 23:02:54 +0200

    test 2

--- auto.ps1
+++ auto.ps1
@@ -8,10 +8,15 @@ git config --global user.email "Anders_RMathiesen@pm.me"
 git config --global core.editor "code --wait"
 
 #installing vscode using winget
-winget install vscode -e
+winget install Microsoft.VisualStudioCode -e
 
-#installing python using winget
-winget install python -e
+#installing latest version of python downloading from python.org
+Invoke-WebRequest https://www.python.org/ftp/python/3.11.3/python-3.11.3-amd64.exe -OutFile python.exe
+./python.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 
 #running python.py script
 python python.py
+
+#installing pip using python
+python -m pip install --upgrade pip
+
--- python.py
+++ python.py
@@ -1,1 +1,28 @@
-#using python to add my ssh key to github account
+#add ssh key to github account using github api
+import requests
+
+def add_ssh_key_to_github(username, token, title, key):
+    url = f"https://api.github.com/user/keys"
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+    data = {
+        "title": title,
+        "key": key
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    if response.status_code == 201:
+        print("SSH key added successfully.")
+    else:
+        print("Failed to add SSH key.")
+        print(f"Response: {response.text}")
+
+# Provide your GitHub username, personal access token, SSH key title, and the actual SSH key content
+username = "your_github_username"
+token = "your_personal_access_token"
+title = "My SSH Key"
+key = "your_ssh_key"
+
+add_ssh_key_to_github(username, token, title, key)
