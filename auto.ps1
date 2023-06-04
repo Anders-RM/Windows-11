53acfa82e558e33004c154d552bf51130d4e84e5
Anders-RM <Anders_RMathiesen@pm.me>
2023-06-05 00:13:34 +0200

    test

--- auto.ps1
+++ auto.ps1
@@ -1,8 +1,8 @@
-#set up ssh key for github assking for passphrase
-ssh-keygen -t rsa -b 4096 -C "Main Key" -f $HOME\.ssh\id_rsa -N '' -q   #create ssh key
+#set up ssh key for github
+ssh-keygen -t rsa -b 4096 -C "Main Key" -f $HOME\.ssh\id_rsa -N '@Ndersraeder' -q   #create ssh key
 
 #insatlling git and setting up git in windows using winget
-winget install git -e
+winget install Git.Git -e
 git config --global user.name "Anders-RM"
 git config --global user.email "Anders_RMathiesen@pm.me"
 git config --global core.editor "code --wait"
@@ -10,9 +10,15 @@ git config --global core.editor "code --wait"
 #installing vscode using winget
 winget install Microsoft.VisualStudioCode -e
 
-#installing latest version of python downloading from python.org
+#installing python version 3.11.3 downloading from python.org
 Invoke-WebRequest https://www.python.org/ftp/python/3.11.3/python-3.11.3-amd64.exe -OutFile python.exe
 ./python.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+
+#run a comand as admin
+Start-Process powershell -Verb runAs -ArgumentList 'iwr -useb https://christitus.com/win | iex'
+
+#installing requests module
+python -m pip install requests  
 
 #running python.py script
 python python.py
--- python.py
+++ python.py
@@ -1,5 +1,6 @@
 #add ssh key to github account using github api
 import requests
+import os
 
 def add_ssh_key_to_github(username, token, title, key):
     url = f"https://api.github.com/user/keys"
@@ -20,11 +21,11 @@ def add_ssh_key_to_github(username, token, title, key):
         print(f"Response: {response.text}")
 
 # Provide your GitHub username, personal access token, SSH key title, and the actual SSH key content
-# file deepcode ignore NoHardcodedCredentials: <please specify a reason of ignoring this>
+# file deepcode ignore NoHardcodedCredentials: <The repository is private an is staying private>
 username = "Anders-RM"
-# file deepcode ignore HardcodedNonCryptoSecret: <please specify a reason of ignoring this>
+# file deepcode ignore HardcodedNonCryptoSecret: <The repository is private an is staying private>
 token = "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"
 title = "Home pc anders@Anders-Win"
-key = "$HOME\.ssh\id_rsa"
+key = os.path.expanduser('~\.ssh\id_rsa')
 
 add_ssh_key_to_github(username, token, title, key)
