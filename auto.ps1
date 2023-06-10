bfd4ea6e13d0abf2df2755ad5ef9d8d27fedfc23
Anders-RM <Anders_RMathiesen@pm.me>
2023-06-10 12:06:36 +0200

    test 10/06/2023 12.06

--- auto.ps1
+++ auto.ps1
@@ -10,6 +10,7 @@ git config --global core.editor "code --wait"
 
 #installing vscode using winget
 winget install Microsoft.VisualStudioCode -e
+winget install Microsoft.VisualStudioCode.Insiders  -e
 
 #installing python version 3.11.3 downloading from python.org
 Invoke-WebRequest https://www.python.org/ftp/python/3.11.3/python-3.11.3-amd64.exe -OutFile python.exe
@@ -19,12 +20,12 @@ Invoke-WebRequest https://www.python.org/ftp/python/3.11.3/python-3.11.3-amd64.e
 $installerPath = ./python.exe
 
 # Start the installer in a separate process
-Start-Process -FilePath $installerPath -ArgumentList -Wait
+Start-Process -FilePath $installerPath -Wait
 
 # Continue with the rest of your script here
 
 #run a Chris Titus Tech's Windows Utility as admin
-Start-Process powershell -Verb runAs -ArgumentList 'iwr -useb https://christitus.com/win | iex'
+Start-Process powershell -Verb runAs -ArgumentList 'iwr -useb https://christitus.com/win | iex' -Wait
 
 #downloading and installing office 365 from https://c2rsetup.officeapps.live.com/c2r/download.aspx?ProductreleaseID=O365ProPlusRetail&platform=x86&language=en-us&version=O16GA
 Invoke-WebRequest "https://c2rsetup.officeapps.live.com/c2r/download.aspx?ProductreleaseID=O365ProPlusRetail&platform=x86&language=en-us&version=O16GA" -OutFile office.exe
@@ -34,16 +35,16 @@ Invoke-WebRequest "https://c2rsetup.officeapps.live.com/c2r/download.aspx?Produc
 $installerPath = ./office.exe
 
 # Start the installer in a separate process
-Start-Process -FilePath $installerPath -ArgumentList -Wait
+Start-Process -FilePath $installerPath -Wait
 
 #run Microsoft Activation Scripts as admin 
-Start-Process powershell -Verb runAs -ArgumentList 'irm https://massgrave.dev/get | iex'
+Start-Process powershell -Verb runAs -ArgumentList 'irm https://massgrave.dev/get | iex' -Wait
 
 #install hyper-v
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart
 
 # Change the default virtual machine file location
-Set-VMHost -VirtualHardDiskPath "C:\VMs" -VirtualMachinePath "C:\VMs"
+#Set-VMHost -VirtualHardDiskPath "C:\VMs" -VirtualMachinePath "C:\VMs"
 
 #customizing windows
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 0
