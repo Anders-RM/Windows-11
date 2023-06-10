422d693a913ec6ce8bcc07f3d3973d628e67ba79
Anders-RM <Anders_RMathiesen@pm.me>
2023-06-10 14:36:57 +0200

    test

--- auto.ps1
+++ auto.ps1
@@ -1,46 +1,43 @@
 Start-Transcript -Path $PSScriptRoot"\powershell.log" -Append -IncludeInvocationHeader
-#set up ssh key for github
+# Set up ssh key for github
 ssh-keygen -t rsa -b 4096 -C "Main Key" -f $HOME\.ssh\id_rsa -N '@Ndersraeder' -q   #create ssh key
 
-#insatlling git and setting up git in windows using winget
+# Insatlling git and setting up git in windows using winget
 winget install Git.Git -e
 
 Start-Process powershell.exe -ArgumentList "-Command", "git config --global user.name 'Anders-RM'" -Wait
 Start-Process powershell.exe -ArgumentList "-Command", "git config --global user.email 'Anders_RMathiesen@pm.me'" -Wait
 
-#installing vscode using winget
+# Installing vscode using winget
 winget install Microsoft.VisualStudioCode -e
 winget install Microsoft.VisualStudioCode.Insiders  -e
 
-#installing python version 3.11.3 downloading from python.org
+# Installing python version 3.11.3 downloading from python.org
 Invoke-WebRequest https://www.python.org/ftp/python/3.11.3/python-3.11.3-amd64.exe -OutFile $PSScriptRoot\python.exe
-
-#./python.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 
 # Start the installer in a separate process
 Start-Process -FilePath $PSScriptRoot\python.exe -Wait
 
-#run a Chris Titus Tech's Windows Utility as admin
+# Run a Chris Titus Tech's Windows Utility as admin
 Start-Process powershell -Verb runAs -ArgumentList 'iwr -useb https://christitus.com/win | iex' -Wait
 
-#downloading and installing office 365 from https://c2rsetup.officeapps.live.com/c2r/download.aspx?ProductreleaseID=O365ProPlusRetail&platform=x86&language=en-us&version=O16GA
+# Downloading and installing office 365 from microsoft
 Invoke-WebRequest "https://c2rsetup.officeapps.live.com/c2r/download.aspx?ProductreleaseID=O365ProPlusRetail&platform=x86&language=en-us&version=O16GA" -OutFile $PSScriptRoot\office.exe
-
-#./office.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 
 # Start the installer in a separate process
 Start-Process -FilePath $PSScriptRoot\office.exe -Wait
 
-#run Microsoft Activation Scripts as admin 
+# Run Microsoft Activation Scripts as admin 
 Start-Process powershell -Verb runAs -ArgumentList 'irm https://massgrave.dev/get | iex' -Wait
 
-#install hyper-v
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart
+# Install hyper-v
+Start-Process powershell.exe -ArgumentList "-Command", "Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart" -Wait
+
 
 # Change the default virtual machine file location
 #Set-VMHost -VirtualHardDiskPath "C:\VMs" -VirtualMachinePath "C:\VMs"
 
-#customizing windows
+# Customizing windows
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 0
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "SearchboxTaskbarMode" -Value 0
 (Get-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Taskband").Favorites | ForEach-Object {Remove-Item $_.Path -Force}
@@ -48,14 +45,12 @@ Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer
 # Enable clapboard history
 Set-PSReadlineOption -HistorySaveStyle SaveIncrementally
 
-#installing requests module
+# Installing requests module
 Start-Process powershell.exe -ArgumentList "-Command", "python -m pip install requests" -Wait
 
-#running python.py script
+# Running python.py script
 Start-Process powershell.exe -ArgumentList "-Command", "python python.py" -Wait
 
-#ristarting computer
-#Restart-Computer -Force
-
 Stop-Transcript
-
+# Ristarting computer
+#Restart-Computer -Force
