e6a603efc71fecb44b1778a3c0163d64f8ecd694
Anders-RM <Anders_RMathiesen@pm.me>
2023-06-10 11:58:44 +0200

    test 10/06/2023 11.58

--- auto.ps1
+++ auto.ps1
@@ -13,14 +13,29 @@ winget install Microsoft.VisualStudioCode -e
 
 #installing python version 3.11.3 downloading from python.org
 Invoke-WebRequest https://www.python.org/ftp/python/3.11.3/python-3.11.3-amd64.exe -OutFile python.exe
-./python.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+#./python.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+
+# Path to the Python installer
+$installerPath = ./python.exe
+
+# Start the installer in a separate process
+Start-Process -FilePath $installerPath -ArgumentList -Wait
+
+# Continue with the rest of your script here
 
 #run a Chris Titus Tech's Windows Utility as admin
 Start-Process powershell -Verb runAs -ArgumentList 'iwr -useb https://christitus.com/win | iex'
 
 #downloading and installing office 365 from https://c2rsetup.officeapps.live.com/c2r/download.aspx?ProductreleaseID=O365ProPlusRetail&platform=x86&language=en-us&version=O16GA
 Invoke-WebRequest "https://c2rsetup.officeapps.live.com/c2r/download.aspx?ProductreleaseID=O365ProPlusRetail&platform=x86&language=en-us&version=O16GA" -OutFile office.exe
-./office.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+#./office.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+
+# Path to the Python installer
+$installerPath = ./office.exe
+
+# Start the installer in a separate process
+Start-Process -FilePath $installerPath -ArgumentList -Wait
+
 #run Microsoft Activation Scripts as admin 
 Start-Process powershell -Verb runAs -ArgumentList 'irm https://massgrave.dev/get | iex'
 
@@ -48,3 +63,4 @@ python python.py
 #Restart-Computer -Force
 
 Stop-Transcript
+
