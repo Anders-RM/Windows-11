847fe7dbcaa86c0862315645ee7a4666cfbea3b6
Anders-RM <Anders_RMathiesen@pm.me>
2023-06-10 12:33:50 +0200

    test 10/06/2023 12.32

--- auto.ps1
+++ auto.ps1
@@ -4,38 +4,32 @@ ssh-keygen -t rsa -b 4096 -C "Main Key" -f $HOME\.ssh\id_rsa -N '@Ndersraeder' -
 
 #insatlling git and setting up git in windows using winget
 winget install Git.Git -e
-git config --global user.name "Anders-RM"
-git config --global user.email "Anders_RMathiesen@pm.me"
-git config --global core.editor "code --wait"
+
+Start-Process powershell.exe -ArgumentList "-Command", "git config --global user.name 'Anders-RM'"
+Start-Process powershell.exe -ArgumentList "-Command", "git config --global user.email 'Anders_RMathiesen@pm.me'"
 
 #installing vscode using winget
 winget install Microsoft.VisualStudioCode -e
 winget install Microsoft.VisualStudioCode.Insiders  -e
 
 #installing python version 3.11.3 downloading from python.org
-Invoke-WebRequest https://www.python.org/ftp/python/3.11.3/python-3.11.3-amd64.exe -OutFile python.exe
+Invoke-WebRequest https://www.python.org/ftp/python/3.11.3/python-3.11.3-amd64.exe -OutFile $PSScriptRoot\python.exe
+
 #./python.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 
-# Path to the Python installer
-$installerPath = ./python.exe
-
 # Start the installer in a separate process
-Start-Process -FilePath $installerPath -Wait
-
-# Continue with the rest of your script here
+Start-Process -FilePath $PSScriptRoot\python.exe -Wait
 
 #run a Chris Titus Tech's Windows Utility as admin
 Start-Process powershell -Verb runAs -ArgumentList 'iwr -useb https://christitus.com/win | iex' -Wait
 
 #downloading and installing office 365 from https://c2rsetup.officeapps.live.com/c2r/download.aspx?ProductreleaseID=O365ProPlusRetail&platform=x86&language=en-us&version=O16GA
-Invoke-WebRequest "https://c2rsetup.officeapps.live.com/c2r/download.aspx?ProductreleaseID=O365ProPlusRetail&platform=x86&language=en-us&version=O16GA" -OutFile office.exe
+Invoke-WebRequest "https://c2rsetup.officeapps.live.com/c2r/download.aspx?ProductreleaseID=O365ProPlusRetail&platform=x86&language=en-us&version=O16GA" -OutFile $PSScriptRoot\office.exe
+
 #./office.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 
-# Path to the Python installer
-$installerPath = ./office.exe
-
 # Start the installer in a separate process
-Start-Process -FilePath $installerPath -Wait
+Start-Process -FilePath $PSScriptRoot\office.exe -Wait
 
 #run Microsoft Activation Scripts as admin 
 Start-Process powershell -Verb runAs -ArgumentList 'irm https://massgrave.dev/get | iex' -Wait
