f41cfa3d2cf80b1588e3d197d74cd0830429897c
Anders-RM <Anders_RMathiesen@pm.me>
2024-04-24 11:25:30 +0200

    add startTest.bat

---
+++ startTest.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Define the PowerShell script path
+set "powerShellScriptPath=%~dp0autoref.ps1"
+
+:: Run PowerShell script as administrator
+powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""!powerShellScriptPath!""' -Verb RunAs}"
+
+endlocal
