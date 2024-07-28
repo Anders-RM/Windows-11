f867cc63c994a546e016bcf9816e9084723918e7
Anders-RM <62700152+Anders-RM@users.noreply.github.com>
2024-07-28 20:05:10 +0200

    Add files via upload

--- Files/BackupScript.ps1
+++ Files/BackupScript.ps1
@@ -1,5 +1,5 @@
 # Specify source and destination directories
-$source = 'C:\Users\Anders\Proton Drive\Anders_RMathiesen\My files\*'
+$source = 'C:\Users\Andsers\filen\*'
 $destinationBase = '\\192.168.3.2\Anders'
 
 # Get the current date and time in the desired format, but replace forbidden characters
@@ -24,8 +24,24 @@ if (Test-Path -Path $logFile) {
 # Start the transcript
 Start-Transcript -Path $logFile
 
+# Define the credentials (replace with actual username and password)
+$username = 'YourUsername'
+$password = ConvertTo-SecureString 'YourPassword' -AsPlainText -Force
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $password
+
+# Use the credential to access the destination path
+New-PSDrive -Name "DestDrive" -PSProvider FileSystem -Root $destinationBase -Credential $credential -Persist
+
+# Create the destination directory if it does not exist on the mapped drive
+if (!(Test-Path -Path "DestDrive:\$date")) {
+    New-Item -ItemType Directory -Path "DestDrive:\$date"
+}
+
 # Copy the files and directories
-Copy-Item -Path $source -Destination $destination -Recurse -Verbose
+Copy-Item -Path $source -Destination "DestDrive:\$date" -Recurse -Verbose
+
+# Remove the PSDrive after copying
+Remove-PSDrive -Name "DestDrive"
 
 # Stop the transcript
 Stop-Transcript
